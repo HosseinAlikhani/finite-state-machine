e2b1a946e437d171f2d35428dbd30e7af8660c4f
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-04-22 20:32:21 +0430

    add: add states

---
+++ src/States/States.php
@@ -0,0 +1,53 @@
+<?php
+namespace Finite\States;
+
+use Finite\Contracts\States\StateInterface;
+use Finite\Contracts\States\StatesInterface;
+use Finite\Exceptions\StateException;
+
+final class States implements StatesInterface
+{
+    /**
+     * store states
+     * @var array
+     */
+    private array $states = [];
+
+    /**
+     * add state to states property
+     * @var StateInterface|string
+     * @return StatesInterface
+     */
+    public function addState(string|StateInterface $state): StatesInterface
+    {
+        $state = $state instanceof StateInterface ? $state : new State($state);
+        $this->states[$state->getName()] = $state;
+        return $this;
+    }
+
+    /**
+     * check state is exists in states property
+     * @var string $stateType
+     * @return bool
+     */
+    public function isStateExists(string $stateType): bool
+    {
+        return key_exists($stateType, $this->states) ? true : false;
+    }
+
+    /**
+     * get specific state from states property
+     * @var string
+     * @return StateInterface
+     */
+    public function getState(string $state): StateInterface
+    {
+        if ( !$this->isStateExists($state) ) {
+            throw new StateException(
+                printf("state %s does not exists", $state),
+                422
+            );
+        }
+        return $this->states[$state];
+    }
+}
