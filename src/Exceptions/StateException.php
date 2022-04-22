e6638c26002d3ad32b144679df3c1cbcdf50654e
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-04-22 20:30:20 +0430

    add: add stateException + interface

---
+++ src/Exceptions/StateException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Finite\Exceptions;
+
+use Exception;
+use Finite\Contracts\Exceptions\StateExceptionInterface;
+
+final class StateException extends Exception implements StateExceptionInterface
+{
+
+}
