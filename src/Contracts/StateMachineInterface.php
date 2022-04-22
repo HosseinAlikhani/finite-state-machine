b600d2d412656650dd15be258ab83d7793745ba7
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-04-22 20:32:53 +0430

    update: add StateMachineInterface with can + setInitializeState + addState methods

---
+++ src/Contracts/StateMachineInterface.php
@@ -0,0 +1,27 @@
+<?php
+namespace Finite\Contracts;
+
+interface StateMachineInterface
+{
+    /**
+     * check state machine can can change state or not
+     * @param string $state
+     * @return bool
+     */
+    public function can(string $state): bool;
+
+    /**
+     * set initialize state to state machine
+     * @param string $state
+     * @return StateMachineInterface
+     */
+    public function setInitializeState(string $state): StateMachineInterface;
+
+    /**
+     * add state to state machine
+     * @param string $stateFrom
+     * @param string $stateTo
+     * @return StateMachineInterface
+     */
+    public function addState(string $stateFrom, string $stateTo): StateMachineInterface;
+}
