cdd3bd9b102514d20ba16851eb2d7b29b17e7f41
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-04-22 20:29:48 +0430

    add: add rules interface + class

---
+++ src/Rules/Rule.php
@@ -0,0 +1,9 @@
+<?php
+namespace Finite\Rules;
+
+use Finite\Contracts\Rules\RuleInterface;
+
+final class Rule implements RuleInterface
+{
+
+}
---
+++ src/Rules/Rules.php
@@ -0,0 +1,9 @@
+<?php
+namespace Finite\Rules;
+
+use Finite\Contracts\Rules\RulesInterface;
+
+final class Rules implements RulesInterface
+{
+
+}
