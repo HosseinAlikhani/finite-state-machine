3272f2af397f256a9e7d81d7185b24e9475ff6b0
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-04-23 15:56:05 +0430

    add: add stateMachine class

---
+++ src/StateMachine.php
@@ -0,0 +1,58 @@
+<?php
+namespace Finite;
+
+use Finite\Contracts\StateMachineInterface;
+use Finite\Contracts\States\StateInterface;
+use Finite\Contracts\States\StatesInterface;
+use Finite\States\States;
+
+class StateMachine implements StateMachineInterface
+{
+    /**
+     * state machines states
+     * @var StatesInterface|null
+     */
+    private StatesInterface|null $states = null;
+
+    /**
+     * store initialize state to state machine
+     * @var StateInterface|null
+     */
+    private StateInterface|null $initializeState = null;
+
+    /**
+     * add state to state machine
+     * @param string $stateFrom
+     * @param array $stateTo
+     * @return StateMachineInterface
+     */
+    public function addState(string $stateFrom, array $stateTo): StateMachineInterface
+    {
+        if ( !$this->states ) {
+            $this->states = new States();
+        }
+        $this->states->addState($stateFrom, $stateTo);
+        return $this;
+    }
+
+    /**
+     * set initialize state to state machine
+     * @param string $state
+     * @return StateMachineInterface
+     */
+    public function setInitializeState(string $state): StateMachineInterface
+    {
+        $this->initializeState = $this->states->getState($state);
+        return $this;
+    }
+
+    /**
+     * check state machine can can change state or not
+     * @param string $state
+     * @return bool
+     */
+    public function can(string $state): bool
+    {
+        return $this->initializeState->canChangeTo($state);
+    }
+}
