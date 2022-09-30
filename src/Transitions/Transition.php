efbc185a5a089a7d5c1470ee0d6abeb3c9c44199
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-09-30 22:18:52 +0330

    update: add constructor for transition class

--- src/Transitions/Transition.php
+++ src/Transitions/Transition.php
@@ -1,9 +1,31 @@
 <?php
 namespace Finite\Transitions;
 
+use Closure;
 use Finite\Transitions\contracts\TransitionInterface;
 
 final class Transition implements TransitionInterface
 {
-    
+    /**
+     * store transition callback
+     * @var array
+     */
+    private array $callback;
+
+    public function __construct(array $transition)
+    {
+        $entry = $do = $exit = null;
+
+        if ( $transition instanceof Closure ) {
+            $entry = $transition;
+        } else {
+            list($entry, $do, $exit ) = $transition;
+        }
+
+        $this->callback = [
+            'entry' =>  $entry,
+            'do'    =>  $do,
+            'exit'  =>  $exit
+        ];
+    }
 }
